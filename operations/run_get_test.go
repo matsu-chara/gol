7522c3a1d29620639de5f598fb1ab0c214ab1150
matsu-chara <matsuy00@gmail.com>
2017-07-12 00:57:07 +0900

    add get operation test

---
+++ operations/run_get_test.go
@@ -0,0 +1,24 @@
+package operations
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCmdGet(t *testing.T) {
+	testFile := tempTest("get")
+	defer os.Remove(testFile)
+
+	err := initDb(testFile)
+	if err != nil {
+		t.Errorf("db init error %s", err)
+	}
+
+	result, err := RunGet(testFile, "k1")
+	if err != nil {
+		t.Errorf("get error %s", err)
+	}
+	if result.Value != "v1" {
+		t.Errorf("unexpected value %s", result)
+	}
+}
