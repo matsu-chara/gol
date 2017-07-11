177f916522964eea6a60dab1fad3cd64f82777af
matsu-chara <matsuy00@gmail.com>
2017-07-12 00:56:48 +0900

    add operation test

---
+++ operations/run_add_test.go
@@ -0,0 +1,28 @@
+package operations
+
+import (
+	"github.com/matsu-chara/gol/kvs"
+	"os"
+	"testing"
+)
+
+func TestCmdAdd(t *testing.T) {
+	testFile := tempTest("add")
+	defer os.Remove(testFile)
+	initDb(testFile)
+
+	RunAdd(testFile, "key_add", "value")
+
+	db, err := kvs.Open(testFile)
+	if err != nil {
+		t.Errorf("kvs open failed %s", err)
+	}
+	result, isExists := db.Get("key_add")
+	if !isExists {
+		t.Error("key_add was not found.")
+	}
+
+	if result.Value != "value" {
+		t.Errorf("%s is not expected key key_add", result)
+	}
+}
