734c46cac6e896c4f5e63484e7c5461b350808a3
matsu-chara <matsuy00@gmail.com>
2017-07-12 00:56:58 +0900

    add dump operation test

---
+++ operations/run_dump_test.go
@@ -0,0 +1,25 @@
+package operations
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestCmdDump(t *testing.T) {
+	testFile := tempTest("dump")
+	defer os.Remove(testFile)
+
+	err := initDb(testFile)
+	if err != nil {
+		t.Errorf("db init error %s", err)
+	}
+	result, err := RunDump(testFile)
+	if err != nil {
+		t.Errorf("dump error %s", err)
+	}
+
+	if !reflect.DeepEqual(result, map[string]string{"k1": "v1", "k2": "v2"}) {
+		t.Errorf("unexpected result %s", result)
+	}
+}
