06d785fcccb88c6aad6f2093aea3612dac8a2650
matsu-chara <matsuy00@gmail.com>
2017-07-12 00:57:16 +0900

    add ls operation test

---
+++ operations/run_ls_test.go
@@ -0,0 +1,28 @@
+package operations
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCmdLs(t *testing.T) {
+	testFile := tempTest("dump")
+	defer os.Remove(testFile)
+
+	err := initDb(testFile)
+	if err != nil {
+		t.Errorf("db init error %s", err)
+	}
+	result, err := RunLs(testFile)
+	if err != nil {
+		t.Errorf("dump error %s", err)
+	}
+
+	if len(result) != 2 {
+		t.Errorf("%s has unexpected length", result)
+	}
+
+	if result[0].Key != "k1" || result[1].Key != "k2" {
+		t.Errorf("unexpected result %s", result)
+	}
+}
