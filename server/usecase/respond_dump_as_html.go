5def1a18590335fe432c157c46859a2f142b5533
matsu-chara <matsuy00@gmail.com>
2017-11-07 00:07:12 +0900

    add post/delete ui

--- server/usecase/respond_dump_as_html.go
+++ server/usecase/respond_dump_as_html.go
@@ -15,11 +15,61 @@ var dumpTemplate = template.Must(template.New("gol").Parse(`
     <title>gol</title>
   </head>
   <body>
-  <ul>
-   {{ range $key, $value := . }}
-   <li>{{ $key }} :<a href="{{ $value }}">{{ $value }}</a></li>
-   {{ end }}
-  </ul>
+	<h3>current links</h3>
+    <ul>
+      {{ range $key, $value := . }}
+      <li>{{ $key }} :<a href="{{ $value }}">{{ $value }}</a></li>
+      {{ end }}
+	</ul>
+	<h3>register new link</h3>
+    <form id="register-form">
+      <label>key: <input id="register-form-key" name="key" type="text" /></label>
+      <label>url: <input id="register-form-value" name="value" type="text" /></label>
+      <input type="button" value="register" onclick="doRegister()">
+	</form>
+	<h3>delete link</h3>
+    <form id="delete-form">
+      <label>key: <input id="delete-form-key" name="key" type="text" /></label>
+      <input type="button" value="delete" onclick="doDelete()">
+	</form>
+    <script type="text/javascript">
+      function doRegister(){
+    	let keyInput = document.getElementById("register-form-key");
+    	let valueInput = document.getElementById("register-form-value");
+		
+		var req = new XMLHttpRequest();
+		req.onreadystatechange = function() {
+		  if (req.readyState == 4) {
+			if (req.status == 201) {
+			  location.reload();
+			} else {	
+			  console.error("registration failed. status: " + req.status + ", response:" + req.response);
+			  alert("registration failed. status: " + req.status + ", response:" + req.response);
+			}
+		  }
+		};
+		req.open("POST", "/" + keyInput.value, true);
+		req.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+		req.send("value=" + encodeURIComponent(valueInput.value));
+    }
+	function doDelete(){
+    	let keyInput = document.getElementById("delete-form-key");
+		
+		var req = new XMLHttpRequest();
+		req.onreadystatechange = function() {
+		  if (req.readyState == 4) {
+			if (req.status == 200) {
+			  location.reload();
+			} else {	
+			  console.error("deletion failed. status: " + req.status + ", response:" + req.response);
+			  alert("deletion failed. status: " + req.status + ", response:" + req.response);
+			}
+		  }
+		};
+		req.open("DELETE", "/" + keyInput.value, true);
+		req.send(null);
+    }
+	</script>
   </body>
 </html>
 `))
